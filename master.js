3406c58f37a26487298b4775cda06b6956d5510c
Orguntay <67823328+Orguntay@users.noreply.github.com>
2022-06-01 23:39:36 +0300

    Add files upload
    Add an optional extentions vs. vs.

---
+++ master.js
@@ -0,0 +1,32 @@
+"use strict";
+function setCookie(cname, cvalue, exdays, domain) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + domain + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+var deger = getCookie("sayac");
+if (deger != "") {
+    document.write(deger + " kere okundu");
+    deger = parseInt(deger) + 1;
+    setCookie("sayac", deger, 10, ".google.com");
+} else {
+    setCookie("sayac", 1, 10, ".google.com");
+}
